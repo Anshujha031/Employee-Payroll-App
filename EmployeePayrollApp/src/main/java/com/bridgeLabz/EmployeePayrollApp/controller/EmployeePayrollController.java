2578d11def972043613ea97f03bce7f8319e04cf
naveenjha031 <naveenjha344@gmail.com>
2025-02-25 12:27:25 +0530

    [Naveen] Add: Http methods or mysql connected to project

--- EmployeePayrollApp/src/main/java/com/bridgeLabz/EmployeePayrollApp/controller/EmployeePayrollController.java
+++ EmployeePayrollApp/src/main/java/com/bridgeLabz/EmployeePayrollApp/controller/EmployeePayrollController.java
@@ -1,15 +1,52 @@
 package com.bridgeLabz.EmployeePayrollApp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bridgeLabz.EmployeePayrollApp.model.Employee;
+import com.bridgeLabz.EmployeePayrollApp.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeePayrollController {
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //GET all employees
     @GetMapping
-    public String getEmployee(){
-        return "Returning Employee Payroll Data";
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    // GET employee by ID
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    //POST - add new employees
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+    //PUT - update employee
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable Long id , @RequestBody Employee updatedEmployee){
+        return employeeRepository.findById(id)
+                .map(employee -> {
+                    employee.setName(updatedEmployee.getName());
+                    employee.setSalary(updatedEmployee.getSalary());
+                    return employeeRepository.save(employee);
+                })
+                .orElse(null);
+    }
+    // DELETE employee
+    @DeleteMapping("/{id}")
+    public String deleteEmployee(@PathVariable Long id) {
+        employeeRepository.deleteById(id);
+        return "Employee deleted successfully";
     }
 }
