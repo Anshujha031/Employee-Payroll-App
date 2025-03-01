7771a73dbbafe77e4e2f0b6857d1d92a2ff68be8
naveenjha031 <naveenjha344@gmail.com>
2025-03-01 12:42:17 +0530

    [Naveen] Add: Adding Logging or feature required UC7

--- EmployeePayrollApp/src/main/java/com/bridgeLabz/EmployeePayrollApp/service/EmployeePayrollService.java
+++ EmployeePayrollApp/src/main/java/com/bridgeLabz/EmployeePayrollApp/service/EmployeePayrollService.java
@@ -3,6 +3,7 @@ package com.bridgeLabz.EmployeePayrollApp.service;
 
 import com.bridgeLabz.EmployeePayrollApp.model.Employee;
 import com.bridgeLabz.EmployeePayrollApp.repository.EmployeeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class EmployeePayrollService {
 
 
@@ -19,11 +21,13 @@ public class EmployeePayrollService {
 
     public List<Employee> getAllEmployees(){
 
+        log.info("Fetching all employee. Total count : {}",employeeList.size() );
         return employeeList;
     }
 
     public Optional<Employee> getEmployeeById(Long id){
 
+        log.info("Fetching employee with ID: {}", id);
         return employeeList.stream().filter(emp -> emp.getId().equals(id)).findFirst();
     }
 
@@ -31,19 +35,28 @@ public class EmployeePayrollService {
 
         employee.setId(employeeIdCounter++); // Assign a unique ID
         employeeList.add(employee);
+        log.info("Created new employee: {}", employee);
         return employee;
     }
 
     public Optional<Employee> updateEmployee(Long id , Employee updatedEmployee){
         return getEmployeeById(id).map(employee -> {
+            log.info("Updating employee with ID: {}", id);
             employee.setName(updatedEmployee.getName());
             employee.setSalary(updatedEmployee.getSalary());
+            log.info("Updated employee details: {}", employee);
             return employee;
         });
     }
 
     public boolean deleteEmployee(Long id) {
-
-        return employeeList.removeIf(employee -> employee.getId().equals(id));
+        log.info("Deleting employee with ID: {}", id);
+        boolean removed = employeeList.removeIf(employee -> employee.getId().equals(id));
+        if (removed) {
+            log.info("Employee with ID {} deleted successfully.", id);
+        } else {
+            log.error("Failed to delete employee with ID {} - Not Found.", id);
+        }
+        return removed;
     }
 }
